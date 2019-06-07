b8c8e19dbe3d60324689e7639bab5faf3b184180
hariperla <haricharan.perla1988@gmail.com>
2019-06-07 14:10:20 -0400

    Update color_to_binary.py
    Updated the file to add both thresholds in the single function

--- color_to_binary.py
+++ color_to_binary.py
@@ -4,10 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
 
-def hls_select(img,thrsh=(120,255)):
+def thresholding(img,color_thrsh=(120,255),gradient_thrsh=(30,100)):
     """
     This function returns the binary output after 
-    applying a color thresholding
+    applying a color thresholding and gradient thresholding
+    using a sobel filter
     Inputs: RGB image
     Output: binary image
     """
@@ -18,47 +19,32 @@ def hls_select(img,thrsh=(120,255)):
     l_channel = hls[:,:,1]
     s_channel = hls[:,:,2]
 
-    binary_output = np.zeros_like(s_channel)
-
-    binary_output[(s_channel > thrsh[0]) & (s_channel <= thrsh[1])] = 1
-
-    return binary_output
-
-def sobel_edge_detect(img,kernel_size):
-    """
-    This function applies a sobel edge detection algorithm
-    on the image to find edges
-    Inputs: Undistorted Image
-    Output: Sobel filtered edge image
-    """
-
-    # Convert to a gray scale image
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    thresh_min = 10
-    thresh_max = 100
-
     # Calculate the sobel magnitudes
-    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0,ksize = kernel_size)
+    sobel_x = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0,ksize = kernel_size)
     abs_sobelx = np.absolute(sobel_x)
-    scaled_sobel = np.uint8(abs_sobelx * 255 / np.max(abs_sobelx))
+    scaled_sobelx = np.uint8(abs_sobelx * 255 / np.max(abs_sobelx))
 
-    # Apply the thresholding and create the binary image
-    edge_output = np.zeros_like(scaled_sobel)
-    edge_output[(scaled_sobel > thresh_min) & (scaled_sobel <= thresh_max)] = 1
+    # Color thresholding
+    color_binary = np.zeros_like(s_channel)
+    color_binary[(s_channel > color_thrsh[0]) & (s_channel <= color_thrsh[1])] = 1
 
-    return edge_output
+    # Gradient thresholding
+    gradient_binary = np.zeros_like(scaled_sobelx)
+    gradient_binary[(scaled_sobelx > gradient_thrsh[0]) & (gradient_thrsh <= thrsh[1])] = 1
 
+    # Combined threshold
+    comb_thrsh_binary = np.zeros_like(gradient_binary)
+    # If any of the pixels in the color threshold image or the gradient image are
+    # true apply it to the combined binary image
+    comb_thrsh_binary[(color_binary == 1) | (gradient_binary ==1)] = 1
+
+    return comb_thrsh_binary
 
 if __name__ == "__main__":
 
     # Read the test image
-    #test_img = mpimg.imread("output_images/test_calibration_after.jpg") 
     test_img = cv2.imread("output_images/test_calibration_after.jpg")
-    sbinary = hls_select(test_img,thrsh=(100,255))
-    sxbinary = sobel_edge_detect(test_img,3)
+    binary_output = thresholding(test_img,color_thrsh=(120,255),gradient_thrsh=(30,100))
 
-    combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(sbinary == 1) & (sxbinary == 1)] = 1
-
-    cv2.imshow("binary image",sbinary)
+    cv2.imshow("binary image",binary_output)
     cv2.waitKey(5000)
